930b193c63f47f5d9785686f69e2f0e2f48265b1
Dmitry Afonchenko <undermove@yandex.ru>
2020-08-07 16:37:53 +0300

    Create producer and consumer

---
+++ OutboxPatternMvp/Producer/PublishController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DotNetCore.CAP;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+
+namespace Producer
+{
+	public class PublishController : Controller
+	{
+		private readonly ICapPublisher _capBus;
+
+		public PublishController(ICapPublisher capPublisher)
+		{
+			_capBus = capPublisher;
+		}
+
+		[Route("~/adonet/transaction")]
+		public IActionResult AdonetWithTransaction()
+		{
+			using (var connection = new MySqlConnection("Server=localhost;Database=outboxmvp;Uid=app;Password=12345;Allow User Variables=True;Port=3308"))
+			{
+				using (var transaction = connection.BeginTransaction(_capBus, autoCommit: true))
+				{
+					//your business logic code
+
+					_capBus.Publish("xxx.services.show.time", DateTime.Now);
+				}
+			}
+
+			return Ok();
+		}
+	}
+}
---
+++ OutboxPatternMvp/Producer/Startup.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Producer
+{
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddCap(x =>
+			{
+				x.UseMySql("Server=localhost;Database=outboxmvp;Uid=app;Password=12345;Allow User Variables=True;Port=3308");
+				x.UseRabbitMQ("rabbitmq://localhost:5673/vhost,username=admin,password=mypass");
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+			});
+		}
+	}
+}
